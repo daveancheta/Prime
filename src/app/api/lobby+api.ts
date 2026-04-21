88d0a43a0322d54b50be36c6c77cb70685d2316a
daveancheta <daveancheta8@gmail.com>
2026-04-22 00:01:43 +0800

    feat: implement POST endpoint for recruitment data submission

--- src/app/api/lobby+api.ts
+++ src/app/api/lobby+api.ts
@@ -17,3 +17,30 @@ export async function GET() {
         })
     }
 }
+
+export async function POST(req: Request) {
+    const { clan_name, tier, region, slot, image, status } = await req.json()
+
+    try {
+        const {error} = await supabase 
+        .from('recruitment')
+        .insert({
+            id: crypto.randomUUID(),
+            clan_name, 
+            tier, 
+            region, 
+            slot, 
+            image, 
+            status,
+            created_by: "2ew52JNIc2IadtR4kLszULvttK77bP9m"
+        })
+         return Response.json({
+            success: true,
+            error
+        })
+    } catch (error) {
+        return Response.json({
+            success: false
+        })
+    }
+}
